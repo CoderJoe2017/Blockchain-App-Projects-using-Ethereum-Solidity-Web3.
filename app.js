524fde7d7a39f9bc82f9c4553e45a5de52d32873
Joe LaPenna <jlapenna2013@gmail.com>
2018-02-03 03:47:09 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const Web3 = require('web3');
+const web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+app.get('/ajax-request', (req, res) => {
+  web3.eth.getAccounts(function(error, addresses){
+    if (error == null) res.send(JSON.stringify(addresses))
+  })
+})
+app.listen(3000);
